21b76c99be2964e0d32daccb2f5379993e1b18c7
PARASHU552 <135123131+PARASHU552@users.noreply.github.com>
2026-03-13 18:26:01 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+app.use(cors());
+const errormiddleware = require("./Middlewares/errormiddleware");
+app.use(express.urlencoded({ extended: true }));
+const bodyParser = require("body-parser");
+app.use(express.json());
+const PORT = process.env.PORT;
+const Mongo_Url = process.env.MONGO_URL;
+const authRoute = require("./Router/AuthRouter");
+const GoalRouter = require("./Router/GoalRouter");
+
+//mongoDB connection
+mongoose
+  .connect(Mongo_Url)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+// Test route
+app.get('/api/health', (req, res) => {
+  res.json({status: 'OK', message: 'Server is running'});
+});
+//route
+app.use("/", authRoute);
+app.use("/", GoalRouter);
+
+app.use(errormiddleware);
+app.listen(PORT, () => {
+  console.log("Server running on port 8000");
+});
+
